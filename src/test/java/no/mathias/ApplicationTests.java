c388db1c6eed8fa57081b98ba1c8d59a5cd05096
Mathias Dahl Andersen <mathias.dahl.andersen@kulturradet.no>
2019-11-29 12:44:50 +0100

    Spring init

---
+++ src/test/java/no/mathias/ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.mathias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
